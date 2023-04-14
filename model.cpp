5b9bf586f13a5b29c2cb70ffbb8e4162fd29893e
JonMercado <jonmercado15@yahoo.com>
2023-04-13 21:13:05 -0600

    Added code to read dialogue from resource file
    Added code that will read text from the json formatted .txt file in the resources folder, allowing the .txt file to be edited directly for ease of adding and referencing multiple tips.

--- model.cpp
+++ model.cpp
@@ -6,6 +6,11 @@ A8: Educational App
 
 #include "model.h"
 #include "car.h"
+#include <QJsonObject>
+#include <QJsonValue>
+#include <QJsonArray>
+#include <QJsonDocument>
+#include <QFileDialog>
 
 Model* Model::instance;
 float Model::PI = acos(-1.0);
@@ -14,11 +19,15 @@ Model::Model(QObject *parent)
     : QObject{parent}
 {
 
+    loadDialogToArray();
+
 }
 
 void Model::init(){
     instance = new Model;
     instance->carModel = new CarModel;
+
+
 }
 
 void Model::runLevelSelect()
@@ -53,7 +62,27 @@ float Model::degToRad(float degree)
 
 void Model::loadDialogToArray()
 {
+    //Do not change from directory. Dialogue can be added into the file, deleted, etc. so long as the file maintains its structure.
+    QString filepath = ":/Dialogue/Resources/Dialogue.txt";
+    QFile fileObj(filepath);
+    fileObj.open(QIODevice::ReadOnly | QIODevice::Text);
+    QByteArray jsonData = fileObj.readAll();
+    fileObj.close();
 
+    QJsonDocument document = QJsonDocument::fromJson(jsonData);
+    QJsonObject object = document.object();
+
+    QJsonValue value = object.value("Dialogue");
+    QJsonArray array = value.toArray();
+    foreach (const QJsonValue & v, array)
+    {
+        QString line = v.toString();
+        dialogue.append(line);
+    }
+    /*
+    foreach (const QString & s, dialogue)
+        qDebug() << s;
+    */
 }
 
 
